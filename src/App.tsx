4a6809cc927ca8a35953c6e277024e29a50f3023
Manida Vilay <vilaymanida@gmail.com>
2022-10-18 16:25:42 +0200

    create digits function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
-import './App.css';
+import { useState } from "react";
+import Digits from "./components/digits/Digits";
+import "./App.scss";
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+
+  // Create digitals
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+
+    return digits;
+  };
+
+  // Update calcul
+  const updateCalc = (value: string) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  // Calculate
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__constructor">
+        <Digits
+          createDigits={createDigits}
+          updateCalc={updateCalc}
+          calculate={calculate}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/digits/Digits.jsx
@@ -0,0 +1,13 @@
+const Digits = (props) => {
+  return (
+    <div className="digits">
+      {props.createDigits()}
+      <button onClick={() => props.updateCalc("0")}>0</button>
+      <button onClick={() => props.updateCalc(".")}>.</button>
+
+      <button onClick={props.calculate}>=</button>
+    </div>
+  );
+};
+
+export default Digits;
